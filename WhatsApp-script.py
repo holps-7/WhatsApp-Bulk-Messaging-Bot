6446edfb9e7fc4cfc5fe245f19616f533f67c39b
Ramesh Sachan <master.ramesh96@gmail.com>
2020-08-08 12:59:31 +0530

    version 0.1
    Update includes the ability to search the contact, if it is in bottom of the list, which was not possible in earlier version

--- WhatsApp-script.py
+++ WhatsApp-script.py
@@ -34,10 +34,13 @@ class WhatsApp:
         browser.implicitly_wait(5)
 
         for i in range(1,len(arr)):
-            receiver = browser.find_element_by_xpath("//span[@title='{}']".format(arr[i][0])).click()
-            message_area = browser.find_element_by_xpath("//* [@id='main']/footer/div[1]/div[2]/div/div[2]")
-            message_area.send_keys(arr[i][1], Keys.ENTER)
-            sleep(0.5)
+            search_area = browser.find_element_by_xpath("//* [@data-tab='3']")
+            search_area.click()
+            search_area.send_keys(arr[i][0])
+            browser.find_element_by_xpath("//span[@title='{}']".format(arr[i][0])).click()
+            browser.find_element_by_xpath("//* [@id='main']/footer/div[1]/div[2]/div/div[2]").send_keys(arr[i][1], Keys.ENTER)
+            #uncomment the line 43 and replace # with any digit of your choice to put a delay of that muany seconds b/w sending 2 messages
+            #sleep(#)
 
         sleep(3)
         browser.close()
